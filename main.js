1bf82fe29b0f44b671a35deee5ec5e3a6b830b79
hakanonay <dr.hakanonay@gmail.com>
2020-04-19 18:24:42 +0200

    benim calısma

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+var dom= document.querySelector('p').innerText
+function op(){
+    alert("welcome")
+    console.log("open a tıkladınız")
+}
+function ab(){
+    alert("Ana Sayfadasınız")
+    console.log("Ana Başlık a tıkladınız")
+}
+function ib(){
+    alert("İkinci Başlıktasınız")
+    console.log("İkinci Başlık a tıkladınız")
+}
+function üb(){
+    alert("Üçüncü Başlıktasınız")
+    console.log("Üçüncü Başlık a tıkladınız")
+}
+function db(){
+    alert("Dördüncü Başlıktasınız")
+    console.log("Dördüncü Başlık a tıkladınız")
+}
+function bb(){
+    alert("Beşinci Başlıktasınız")
+    console.log("Beşinci Başlık a tıkladınız")
+}
+function altb(){
+    alert("Altıncı Başlıktasınız")
+    console.log("Altıncı Başlık a tıkladınız")
+}
+function bby(){
+    alert("Kalbur Saman İçinde")
+    console.log("Evvel Zaman İçinde")
+}
