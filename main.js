11970c6266ea7146f34702ec31bc8a84dc02c90a
hakanonay <dr.hakanonay@gmail.com>
2020-04-20 12:09:20 +0200

    hakan calisma

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 
 
 var dom= document.querySelector('p').innerText
-function op(){
-    alert("welcome")
-    console.log("open a tıkladınız")
+
+function st(){
+    var name= document.querySelector('.nameInput').value;
+    alert(name);
+    var email= document.querySelector('.emailInput').value;
+    alert(email);
+    var idnummer= document.querySelector('.idInput').value;
+    alert(idnummer);
+    var adresse= document.querySelector('.adresseInput').value;
+    alert(adresse);
+    var ort= document.querySelector('.ortInput').value;
+    alert(ort);
+    var land= document.querySelector('.landInput').value;
+    alert(land);
+    var iban= document.querySelector('.ibanInput').value;
+    alert(iban);
+    var all='name:'+ name + "email" + email + "idnummer" + idnummer + "adresse" + adresse + "ort" + ort + "land" + land + "iban" + iban 
+    alert(all)
+    console.log("Submit Tiklandi")
 }
+
 function ab(){
-    alert("Ana Sayfadasınız")
+    alert("Ana Baslikasiniz")
     console.log("Ana Başlık a tıkladınız")
 }
 function ib(){
@@ -32,4 +49,8 @@ function altb(){
 function bby(){
     alert("Kalbur Saman İçinde")
     console.log("Evvel Zaman İçinde")
+}
+function op(){
+    alert("welcome")
+    console.log("open a tıkladınız")
 }
